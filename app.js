a5125f87e7edd9e285df7065745b3e0449ee9b1d
Jasmine <jasmine@Jasmines-MacBook-Pro.local>
2018-03-05 17:16:07 -0600

    got the news!

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require( 'express' );
 const app = express(); // creates an instance of an express application
 
-app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
+
+app.get('/', function (req, res) {
+  res.send('Wiki home page');
+});
+
+app.get('/news', function (req, res) {
+  res.send('About this news');
+});
+
